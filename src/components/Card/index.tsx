9f6626a3a43717e13c4a922e175800e8c228f20e
Rafael Nunes <rafael.nunes@softdesign.com.br>
2022-07-12 16:29:29 -0300

    add card component

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,8 @@
+import React, { HTMLAttributes } from "react";
+import "./styles.scss";
+
+type Props = HTMLAttributes<HTMLDivElement>;
+
+export function Card({className, ...props}: Props) {
+  return <div className={`card ${className}` } {...props}/>
+}
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -3,9 +3,12 @@ import { Button } from "../../components/Button";
 import { Grid } from "../../components/Grid";
 import { Heading } from "../../components/Heading";
 import { Input } from "../../components/Input";
+import { Card } from "../../components/Card";
+
+import "./styles.scss"
 
 export function Login() {
   return (
-    <Grid><Heading>eae meu querido</Heading></Grid>
+    <Grid><Card></Card></Grid>
   )
 }

9f044dc206e2fc1f59793d4aad0307bd7a5a4b5d
Rafael Nunes <rafael.nunes@softdesign.com.br>
2022-07-13 11:27:19 -0300

    add details page

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,12 +1,16 @@
-
 import { ButtonHTMLAttributes } from "react";
 import "./styles.scss";
 
-type Props = { 
-  variant?: "default" | "outlined";
+type Props = {
+  variant?: "default" | "outlined" | "transparent";
   size?: "md" | "lg";
-} & ButtonHTMLAttributes<HTMLButtonElement> ;
+} & ButtonHTMLAttributes<HTMLButtonElement>;
 
-export function Button({ variant="default", size="md", className, ...props}: Props) {
-  return <button  {...props} className={`${variant} ${size} ${className}`}/>
+export function Button({
+  variant = "default",
+  size = "md",
+  className,
+  ...props
+}: Props) {
+  return <button {...props} className={`${variant} ${size} ${className}`} />;
 }
--- src/pages/Details/index.tsx
+++ src/pages/Details/index.tsx
@@ -1,8 +1,52 @@
-import React from "react";
-import { useParams, useLocation } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import "./styles.scss";
+import { useLocation, useNavigate } from "react-router-dom";
+import { Card } from "../../components/Card";
+import { Grid } from "../../components/Grid";
+import { Heading } from "../../components/Heading";
+import { ReactComponent as CloseIcon } from "../../icons/close.svg";
+import { Button } from "../../components/Button";
+
+type State = {
+  selectedDragon?: Dragon;
+};
 
 export function Details() {
+  const navigate = useNavigate();
+  const [selectedDragon, setSelectedDragon] = useState<Dragon | null>(null);
   const { state } = useLocation();
-  console.log(state);
-  return <h1> {"eae"} </h1>;
+
+  useEffect(() => {
+    const typedState = state as State;
+    if (typedState?.selectedDragon) {
+      setSelectedDragon(typedState.selectedDragon);
+    } else {
+      navigate("/list");
+    }
+  }, []);
+
+  return (
+    <Grid>
+      <div className="container">
+        <Card className="details-card">
+          <Button
+            variant="transparent"
+            className="icon-button"
+            onClick={() => navigate(-1)}
+          >
+            <CloseIcon className="icon" />
+          </Button>
+          <Heading className="title">Detalhes do dragão</Heading>
+          <h2 className="label">Data de criação:</h2>
+          <span className="information">
+            {String(selectedDragon?.createdAt)}
+          </span>
+          <h2 className="label">Nome:</h2>
+          <span className="information">{selectedDragon?.name}</span>
+          <h2 className="label">Tipo:</h2>
+          <span className="information">{selectedDragon?.type}</span>
+        </Card>
+      </div>
+    </Grid>
+  );
 }

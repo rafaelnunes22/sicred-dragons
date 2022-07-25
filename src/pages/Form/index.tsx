3b664f7edc487e0206465e3646cb4a4ddb8973eb
Rafael Nunes <rafael.nunes@softdesign.com.br>
2022-07-25 13:53:40 -0300

    add loader to form

--- src/pages/Form/index.tsx
+++ src/pages/Form/index.tsx
@@ -10,6 +10,8 @@ import { Message } from "../../components/Message";
 import { ReactComponent as CloseIcon } from "../../icons/close.svg";
 import { generateTimeoutMessage } from "../../utils/utils";
 import { createDragon, getDragonById, updateDragon } from "../../api";
+import { useLoading } from "../../contexts/LoadingContext";
+import { Loader } from "../../components/Loader";
 
 export function Form() {
   const navigate = useNavigate();
@@ -20,12 +22,14 @@ export function Form() {
   const [message, setMessage] = useState<string | null>(null);
 
   const { state } = useLocation();
+  const { loading, setLoading } = useLoading();
 
   useEffect(() => {
     const typedState = state as State;
     if (typedState?.dragonId) {
       setDragonId(typedState?.dragonId);
       const internalGetDragonById = async (id: string) => {
+        setLoading(true);
         const response = await getDragonById(id);
         if (response.error) {
           alert(response.error);
@@ -34,13 +38,15 @@ export function Form() {
           setName(response.data.name);
           setType(response.data.type);
         }
+        setLoading(false);
       };
 
       internalGetDragonById(typedState.dragonId);
     }
-  }, [navigate, state]);
+  }, [navigate, state, setLoading]);
 
   const create = useCallback(async () => {
+    setLoading(true);
     const response = await createDragon({ name, type });
 
     if (response.error) {
@@ -50,9 +56,11 @@ export function Form() {
       setError(false);
       generateTimeoutMessage(response.message!, setMessage);
     }
-  }, [name, type, setMessage]);
+    setLoading(false);
+  }, [name, type, setMessage, setLoading]);
 
   const update = useCallback(async () => {
+    setLoading(true);
     const response = await updateDragon({ name, type }, dragonId as string);
 
     if (response.error) {
@@ -62,7 +70,8 @@ export function Form() {
       setError(false);
       generateTimeoutMessage(response.message!, setMessage);
     }
-  }, [name, type, dragonId, setMessage]);
+    setLoading(false);
+  }, [name, type, dragonId, setMessage, setLoading]);
 
   return (
     <Grid>
@@ -71,41 +80,50 @@ export function Form() {
           <Button variant="transparent" onClick={() => navigate("/list")}>
             <CloseIcon />
           </Button>
-          <Heading className={styles.title}>
-            {dragonId ? "Edite" : "Cadastre"} seu dragão
-          </Heading>
-          <form
-            onSubmit={(e) => {
-              e.preventDefault();
-              if (!name || !type) {
-                setMessage("Preencha todos os campos");
-                setError(true);
-                setTimeout(() => {
-                  setMessage(null);
-                }, 3000);
-              } else {
-                dragonId ? update() : create();
-              }
-            }}
-          >
-            <Input
-              placeholder="Nome"
-              onChange={(e) => setName(e.target.value)}
-              value={name}
-            />
-            <Input
-              placeholder="Tipo"
-              onChange={(e) => setType(e.target.value)}
-              value={type}
-            />
-            <Button type="submit" style={{ marginTop: 15 }}>
-              {dragonId ? "Editar" : "Cadastrar"}
-            </Button>
-          </form>
 
-          {message ? (
-            <Message variant={error ? "error" : "default"}>{message}</Message>
-          ) : null}
+          {loading ? (
+            <Loader />
+          ) : (
+            <>
+              <Heading className={styles.title}>
+                {dragonId ? "Edite" : "Cadastre"} seu dragão
+              </Heading>
+              <form
+                onSubmit={(e) => {
+                  e.preventDefault();
+                  if (!name || !type) {
+                    setMessage("Preencha todos os campos");
+                    setError(true);
+                    setTimeout(() => {
+                      setMessage(null);
+                    }, 3000);
+                  } else {
+                    dragonId ? update() : create();
+                  }
+                }}
+              >
+                <Input
+                  placeholder="Nome"
+                  onChange={(e) => setName(e.target.value)}
+                  value={name}
+                />
+                <Input
+                  placeholder="Tipo"
+                  onChange={(e) => setType(e.target.value)}
+                  value={type}
+                />
+                <Button type="submit" style={{ marginTop: 15 }}>
+                  {dragonId ? "Editar" : "Cadastrar"}
+                </Button>
+              </form>
+
+              {message ? (
+                <Message variant={error ? "error" : "default"}>
+                  {message}
+                </Message>
+              ) : null}
+            </>
+          )}
         </Card>
       </div>
     </Grid>
